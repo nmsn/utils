f9cbb3f0297f78767072a99f3e63aaa2616bcb9e
nmsn <136696700@qq.com>
2022-05-30 17:06:01 +0800

    feat(validator): 优化校验函数

---
+++ src/validator/index.ts
@@ -0,0 +1,45 @@
+import regex from '@/regex';
+// import regex from '../regex/index';
+
+const isNumOrStr = (value: number | string): value is string | number => {
+  return typeof value === 'number' || typeof value === 'string';
+};
+
+/** 校验是否为有效数据
+ * @param {number|string} value
+ * @returns {boolean} Judge value is valid param.
+ */
+export const isValidParam = (value: number | string): boolean => {
+  if (!isNumOrStr(value)) {
+    return false;
+  }
+
+  if (typeof value === 'number' && isNaN(value)) {
+    return false;
+  }
+
+  if (typeof value === 'string' && value.trim() === '') {
+    return false;
+  }
+
+  return true;
+};
+
+export const isValidEnum = (value: number | string) => {
+  if (typeof value === 'string') {
+    return isValidParam(value);
+  }
+
+  return typeof value === 'number' && value >= 0;
+};
+
+export const isValidMoney = (value: number | string) => {
+  const val = typeof value === 'string' ? value : value.toString();
+  return regex.money.test(val);
+};
+
+export default {
+  isValidParam,
+  isValidEnum,
+  isValidMoney,
+};

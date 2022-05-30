3fb9e3352f7213f5f724ef6e80e9be52251799ac
nmsn <136696700@qq.com>
2022-05-30 17:18:12 +0800

    chore(directory): export function

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { default as validator } from './validator';
+export { default as regex } from './regex';
---
+++ src/regex/index.ts
@@ -0,0 +1,8 @@
+export const cnName = /^[\u4e00-\u9fa5·]+$/;
+
+export const money = /^-?\d+(,\d{3})*(\.\d{1,2})?$/;
+
+export default {
+  cnName,
+  money,
+};

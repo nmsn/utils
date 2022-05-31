93079ebb7cdb5b2bc3c38b5d22dd45085beb1959
nmsn <136696700@qq.com>
2022-05-31 20:19:19 +0800

    test(validator): 更改校验模式

---
+++ __tests__/validator/index.test.ts
@@ -0,0 +1,60 @@
+import { isValidParam, isValidEnum, isValidMoney } from '@/validator/index';
+
+const isValidParamTestArr = [
+  [0, true],
+  [1, true],
+  [-1, true],
+  [Infinity, true],
+  [-Infinity, true],
+  [NaN, false],
+  ['', false],
+  [' ', false],
+  ['123', true],
+  ['a', true],
+] as [string | number, boolean][];
+
+describe('isValidParam', () => {
+  it.each(isValidParamTestArr)('"%s" is valid param', (val, bool) => {
+    expect(isValidParam(val)).toBe(bool);
+  });
+});
+
+const isValidEnumTestArr = [
+  [0, true],
+  [1, true],
+  [-1, false],
+  [Infinity, true],
+  [-Infinity, false],
+  [NaN, false],
+  ['', false],
+  [' ', false],
+  ['123', true],
+  ['a', true],
+] as [string | number, boolean][];
+
+describe('isValidEnum', () => {
+  it.each(isValidEnumTestArr)('"%s" is valid enum', (val, bool) => {
+    expect(isValidEnum(val)).toBe(bool);
+  });
+});
+
+// ---
+
+const isValidMoneyTestArr = [
+  [0, true],
+  [1, true],
+  [-1, true],
+  [Infinity, false],
+  [-Infinity, false],
+  [NaN, false],
+  ['', false],
+  [' ', false],
+  ['123', true],
+  ['a', false],
+] as [string | number, boolean][];
+
+describe('isValidMoney', () => {
+  it.each(isValidMoneyTestArr)('"%s" is valid money', (val, bool) => {
+    expect(isValidMoney(val)).toBe(bool);
+  });
+});
--- src/validator/index.ts
+++ src/validator/index.ts
@@ -34,6 +34,9 @@ export const isValidEnum = (value: number | string) => {
 };
 
 export const isValidMoney = (value: number | string) => {
+  if (typeof value === 'number' && [Infinity, -Infinity].includes(value)) {
+    return false;
+  }
   const val = typeof value === 'string' ? value : value.toString();
   return regex.money.test(val);
 };

6c7469ddb4a96045e29bbad14321726772d8a63e
nmsn <136696700@qq.com>
2022-07-03 22:31:53 +0800

    feat(hooks): add some base hooks

---
+++ src/hooks/useUnmount.ts
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import useLatest from './useLatest';
+
+const useUnmount = (fn: () => void) => {
+  const fnRef = useLatest(fn);
+
+  useEffect(
+    () => () => {
+      fnRef.current();
+    },
+    [],
+  );
+};
+
+export default useUnmount;

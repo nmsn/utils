8529062b4d4eec1ea23f9477f39fe0122527543a
nmsn <136696700@qq.com>
2022-08-09 16:01:33 +0800

    chore(format): 删除无用代码

--- src/format/index.ts
+++ src/format/index.ts
@@ -10,7 +10,11 @@ export const formatSectionStr = (data: string | string[], sign: string) => {
   return filterArr.join(sign);
 };
 
-const arr2Obj = (data: [string, any][]) => {
+export const arr2Obj = (data: [string, any][]) => {
+  if (!isValidArray(data)) {
+    return {};
+  }
+
   return Object.assign(
     {},
     ...Array.from(
@@ -21,7 +25,11 @@ const arr2Obj = (data: [string, any][]) => {
   );
 };
 
-export const displayWithUnit = (num: number | string, unit: string) => {
+export const displayWithUnit = (
+  num: number | string,
+  unit: string,
+  emptyMark = EMPTY_TEXT_MARK,
+) => {
   if (typeof num === 'number' && (num || num === 0)) {
     return `${num}${unit}`;
   }
@@ -35,205 +43,7 @@ export const displayWithUnit = (num: number | string, unit: string) => {
     return `${num}${unit}`;
   }
 
-  return EMPTY_TEXT_MARK;
-};
-
-const defaultFilterKeys = ['Str', 'Show', 'Desc'];
-
-const hasFilterKeys = (key: string) => {
-  return defaultFilterKeys.some(item => key.includes(item));
-};
-
-const getParamKeys = (
-  paramKeys: string[],
-): [string[], { globalParamKeys: string[]; nextParamsKeysMap: Map<any, any> }] => {
-  const globalParamKeys: string[] = [];
-  const curParamKeys: string[] = [];
-  const nextParamsKeysMap = new Map();
-
-  paramKeys.forEach(item => {
-    if (item.includes('.')) {
-      const keys = item.split('.').filter(Boolean);
-
-      if (keys.length === 1) {
-        curParamKeys.push(keys[0]);
-      } else {
-        const [curKey, ...nextKeysArr] = keys;
-        const nextKeysStr = `.${nextKeysArr.join('.')}`;
-        if (nextParamsKeysMap.has(curKey)) {
-          nextParamsKeysMap.set(curKey, [...nextParamsKeysMap.get(curKey), nextKeysStr]);
-        } else {
-          nextParamsKeysMap.set(curKey, [nextKeysStr]);
-        }
-      }
-    } else {
-      // 全局过滤属性
-      globalParamKeys.push(item);
-    }
-  });
-
-  return [
-    [...curParamKeys, ...globalParamKeys],
-    {
-      globalParamKeys,
-      nextParamsKeysMap,
-    },
-  ];
-};
-
-const getNewParamKeys = (
-  key: string,
-  nextParam: {
-    globalParamKeys: string[];
-    nextParamsKeysMap: Map<any, any>;
-  },
-) => {
-  const { globalParamKeys, nextParamsKeysMap } = nextParam;
-
-  const extraParamKeys: string[] = nextParamsKeysMap.get(key) || [];
-  const newParamKeys = [...globalParamKeys, ...extraParamKeys];
-
-  return newParamKeys;
-};
-
-const getNewConfig = (
-  config: { [key: string]: any },
-  editedConfig: {
-    passKeys?: string[];
-    numKeys?: string[];
-    strKeys?: string[];
-    arrKeys?: string[];
-  },
-) => {
-  return {
-    ...config,
-    ...editedConfig,
-  };
-};
-
-export const initFormatData = (
-  data: { [key: string]: any },
-  config?: {
-    numKeys?: string[];
-    passKeys?: string[];
-  },
-) => {
-  const result: Record<string, any> = {};
-  const { numKeys = [], passKeys = [] } = config || {};
-
-  const [curPassKeys, nextPassParam] = getParamKeys(passKeys);
-  const [curNumKeys, nextNumParam] = getParamKeys(numKeys);
-
-  Object.entries(data).forEach(([key, value]) => {
-    if (curPassKeys.includes(key) || hasFilterKeys(key)) {
-      // 过滤属性不要
-    } else if (value === null || value === -1 || value === '') {
-      result[key] = undefined;
-    } else if (curNumKeys.includes(key)) {
-      if (Array.isArray(value)) {
-        result[key] = value?.map(item => item?.toString());
-      } else {
-        result[key] = value?.toString();
-      }
-    } else if (
-      Array.isArray(value) &&
-      value?.every(item => Object.prototype.toString.call(item) === '[object Object]')
-    ) {
-      const newPassKeys = getNewParamKeys(key, nextPassParam);
-      const newNumKeys = getNewParamKeys(key, nextNumParam);
-      const newConfig = getNewConfig(config || {}, { passKeys: newPassKeys, numKeys: newNumKeys });
-      result[key] = value.map(item => initFormatData(item, newConfig));
-    } else if (Object.prototype.toString.call(value) === '[object Object]') {
-      const newPassKeys = getNewParamKeys(key, nextPassParam);
-      const newNumKeys = getNewParamKeys(key, nextNumParam);
-      const newConfig = getNewConfig(config || {}, { passKeys: newPassKeys, numKeys: newNumKeys });
-      result[key] = initFormatData(value, newConfig);
-    } else {
-      result[key] = value;
-    }
-  });
-
-  return result;
-};
-
-export const submitFormatData = (
-  data: { [key: string]: any },
-  config?: {
-    passKeys?: string[];
-    numKeys?: string[];
-    strKeys?: string[];
-    arrKeys?: string[];
-  },
-) => {
-  const result: Record<string, any> = {};
-  const { passKeys = [], numKeys = [], strKeys = [], arrKeys = [] } = config || {};
-
-  const [curPassKeys, nextPassParam] = getParamKeys(passKeys);
-  const [curNumKeys, nextNumParam] = getParamKeys(numKeys);
-  const [curStrKeys, nextStrParam] = getParamKeys(strKeys);
-  const [curArrKeys, nextArrParam] = getParamKeys(arrKeys);
-
-  Object.entries(data).forEach(([key, value]) => {
-    if (
-      curPassKeys.includes(key) ||
-      key.includes('Str') ||
-      key.includes('Desc') ||
-      key.includes('Show')
-    ) {
-      // 过滤属性不要
-    } else if (curNumKeys.includes(key)) {
-      if (Array.isArray(value)) {
-        result[key] = value?.map(item => (Number(item) || Number(item) === 0 ? Number(item) : -1));
-      } else {
-        result[key] = Number(value) || Number(value) === 0 ? Number(value) : -1;
-      }
-    } else if (curStrKeys.includes(key)) {
-      if (Array.isArray(value)) {
-        result[key] = value?.map(item => item?.toString() || '');
-      } else {
-        result[key] = value?.toString() || '';
-      }
-    } else if (curArrKeys.includes(key)) {
-      result[key] = Array.isArray(value) ? value : [];
-    } else if (value === undefined) {
-      result[key] = -1;
-    } else if (
-      Array.isArray(value) &&
-      value?.every(item => Object.prototype.toString.call(item) === '[object Object]')
-    ) {
-      const newPassKeys = getNewParamKeys(key, nextPassParam);
-      const newNumKeys = getNewParamKeys(key, nextNumParam);
-      const newStrKeys = getNewParamKeys(key, nextStrParam);
-      const newArrKeys = getNewParamKeys(key, nextArrParam);
-
-      const newConfig = getNewConfig(config || {}, {
-        passKeys: newPassKeys,
-        numKeys: newNumKeys,
-        strKeys: newStrKeys,
-        arrKeys: newArrKeys,
-      });
-
-      result[key] = value.map(item => submitFormatData(item, newConfig));
-    } else if (Object.prototype.toString.call(value) === '[object Object]') {
-      const newPassKeys = getNewParamKeys(key, nextPassParam);
-      const newNumKeys = getNewParamKeys(key, nextNumParam);
-      const newStrKeys = getNewParamKeys(key, nextStrParam);
-      const newArrKeys = getNewParamKeys(key, nextArrParam);
-
-      const newConfig = getNewConfig(config || {}, {
-        passKeys: newPassKeys,
-        numKeys: newNumKeys,
-        strKeys: newStrKeys,
-        arrKeys: newArrKeys,
-      });
-
-      result[key] = submitFormatData(value, newConfig);
-    } else {
-      result[key] = value;
-    }
-  });
-
-  return result;
+  return emptyMark;
 };
 
 export const getSuffix = (text: string) => {
@@ -245,7 +55,7 @@ export const getSuffix = (text: string) => {
 export const addThousandSeparator = (num: number | string) => {
   // 不符合规范的数字
   if (!isValidMoney(num)) {
-    return null;
+    return '0';
   }
 
   if (typeof num === 'number') {

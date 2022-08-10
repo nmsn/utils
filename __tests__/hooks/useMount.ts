75ef5a3d70feca4205975cb8e8a5946452f8b5f7
nmsn <136696700@qq.com>
2022-08-10 16:57:10 +0800

    test(hook): add test case

---
+++ __tests__/hooks/useMount.ts
@@ -0,0 +1,19 @@
+import { renderHook } from '@testing-library/react';
+import useMount from '@/hooks/useMount';
+
+describe('useMount', () => {
+  it('test mount', async () => {
+    const fn = jest.fn();
+    const { rerender, unmount } = renderHook(() => useMount(fn));
+    expect(fn).toBeCalledTimes(1);
+
+    rerender();
+    expect(fn).toBeCalledTimes(1);
+
+    unmount();
+    expect(fn).toBeCalledTimes(1);
+
+    renderHook(() => useMount(fn)).unmount();
+    expect(fn).toBeCalledTimes(2);
+  });
+});
---
+++ __tests__/hooks/useUnmount.ts
@@ -0,0 +1,16 @@
+import { renderHook } from '@testing-library/react';
+import useUnmount from '@/hooks/useUnmount';
+
+describe('useUnmount', () => {
+  it('useUnmount should work', async () => {
+    const fn = jest.fn();
+    const { rerender, unmount } = renderHook(() => useUnmount(fn));
+    expect(fn).toBeCalledTimes(0);
+
+    rerender();
+    expect(fn).toBeCalledTimes(0);
+
+    unmount();
+    expect(fn).toBeCalledTimes(1);
+  });
+});

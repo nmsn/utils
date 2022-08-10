ba62e924f5832111fc2cf8a6b3f197aef80bf437
nmsn <136696700@qq.com>
2022-08-10 16:50:54 +0800

    ci(jest): 更改 jest 测试环境

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   rootDir: './',
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',

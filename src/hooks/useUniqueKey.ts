180ec9f5bb3470cab897a3d5b45ea808082428b9
nmsn <136696700@qq.com>
2022-08-11 17:16:37 +0800

    feat(hook): add useUniqueKey hook

--- src/hooks/useUniqueKey.ts
+++ src/hooks/useUniqueKey.ts
@@ -1,16 +1,18 @@
 import { useCallback, useRef } from 'react';
 
 const useUniqueKey = (suffix: string) => {
-  const keys = useRef(new Set());
+  const keys = useRef(new Map());
 
   const getUniqueKey = useCallback((key: string) => {
-    let curKey = key;
-    if (keys.current.has(curKey)) {
-      curKey = `${curKey}-${suffix}`;
+    let num = 0;
+
+    if (keys.current.has(key)) {
+      num = keys.current.get(key);
     }
 
-    keys.current = new Set([...keys.current, curKey]);
-    return key;
+    keys.current.set(key, num + 1);
+
+    return `${key}${`-${suffix}`.repeat(num)}`;
   }, []);
 
   return [getUniqueKey];

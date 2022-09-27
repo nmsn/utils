72e4894ab14c63204242f590450b3a23c4cef914
nmsn <136696700@qq.com>
2022-09-27 15:30:35 +0800

    feat(hooks): add usePrevious

---
+++ src/__tests__/hooks/usePrevious.test.ts
@@ -0,0 +1,20 @@
+import { renderHook } from '@testing-library/react';
+import usePrevious from '../../hooks/usePrevious';
+
+describe('usePrevious', () => {
+  it('test usePrevious', async () => {
+    const { rerender, result } = renderHook(({ state }) => usePrevious(state), {
+      initialProps: { state: 0 },
+    });
+
+    const preVal = result.current;
+
+    expect(preVal).toBe(undefined);
+
+    rerender({ state: 1 });
+
+    const preVal2 = result.current;
+
+    expect(preVal2).toBe(0);
+  });
+});

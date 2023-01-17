e17df8cb5b0d0960b7bd249dbc470022a482c06d
nmsn <136696700@qq.com>
2023-01-17 17:11:36 +0800

    feat(hooks): add useStateCallback

---
+++ src/__tests__/hooks/useStateCallback.test.ts
@@ -0,0 +1,29 @@
+import { renderHook, act } from '@testing-library/react';
+import useStateCallback from '../../hooks/useStateCallback';
+
+describe('useStateCallback', () => {
+  it('test useStateCallback', async () => {
+    const fn = jest.fn(data => data);
+    const { result } = renderHook(({ state }) => useStateCallback(state), {
+      initialProps: { state: 0 },
+    });
+
+    const [state, setStateCallback] = result.current;
+
+    expect(fn).toBeCalledTimes(0);
+
+    act(() => {
+      setStateCallback(1, fn);
+    });
+
+    expect(fn).toBeCalledTimes(1);
+    fn.mockResolvedValue(1);
+
+    act(() => {
+      setStateCallback(10, fn);
+    });
+
+    expect(fn).toBeCalledTimes(2);
+    fn.mockResolvedValue(10);
+  });
+});

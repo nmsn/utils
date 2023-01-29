610327ec2f4f47003b7aa64d9ca307c6f6475883
nmsn <136696700@qq.com>
2023-01-29 20:09:36 +0800

    test(format): 补充测试用例

--- src/format/addThousandSeparator/index.test.ts
+++ src/format/addThousandSeparator/index.test.ts
@@ -5,6 +5,7 @@ it('addThousandSeparator', () => {
   expect(addThousandSeparator('0')).toBe('0');
   expect(addThousandSeparator(0)).toBe('0');
   expect(addThousandSeparator(10000)).toBe('10,000');
+  expect(addThousandSeparator(-10000)).toBe('-10,000');
   expect(addThousandSeparator(1234.5)).toBe('1,234.5');
   expect(addThousandSeparator('1234.5')).toBe('1,234.5');
 });

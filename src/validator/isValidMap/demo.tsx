bb5701286ebacec85b41278ce719f5f6c625cb5c
nmsn <136696700@qq.com>
2023-02-05 20:06:37 +0800

    feat(validator): add isValidMap dumi

---
+++ src/validator/isValidMap/demo.tsx
@@ -0,0 +1,20 @@
+import { isValidMap } from '@nmsn/utils';
+import React from 'react';
+
+export default () => {
+  const source = new Map([]);
+  const result = isValidMap(source);
+  const source2 = new Map([
+    [1, 1],
+    [2, 2],
+  ]);
+  const result2 = isValidMap(source2);
+  return (
+    <div>
+      <p>source: {JSON.stringify(source)}</p>
+      <p>result: {JSON.stringify(result)}</p>
+      <p>source2: {JSON.stringify(source2)}</p>
+      <p>result2: {JSON.stringify(result2)}</p>
+    </div>
+  );
+};
---
+++ src/validator/isValidMap/index.md
@@ -0,0 +1,29 @@
+---
+title: isValidMap
+toc: false
+---
+
+## isValidMap
+
+判断参数是否为有效的 `Map`（含有键值对）
+
+<code src="./demo.tsx"></code>
+
+### API
+
+```typescript
+const flag: boolean = isValidMap(param: unknown);
+```
+
+### Params
+
+| 参数  | 说明 | 类型  | 默认值 |
+| ----- | ---- | ----- | ------ |
+| param | 参数 | `unknown` | -      |
+
+
+### Result
+
+| 参数 | 说明         | 类型      | 默认值  |
+| ---- | ------------ | --------- | ------- |
+| flag | 是否为有效的 'Map' | `boolean` | `false` |

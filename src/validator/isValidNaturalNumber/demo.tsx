e8483400fa7ddf9dad9e68dc0fb337a1e80be492
nmsn <136696700@qq.com>
2023-02-06 21:24:20 +0800

    feat(validator): add isValidNaturalNumber dumi

---
+++ src/validator/isValidNaturalNumber/demo.tsx
@@ -0,0 +1,20 @@
+import { isValidNaturalNumber } from '@nmsn/utils';
+import React from 'react';
+
+export default () => {
+  const source = 1;
+  const result = isValidNaturalNumber(source);
+
+  const source2 = -1;
+  const result2 = isValidNaturalNumber(source2);
+
+  return (
+    <div>
+      <p>source: {JSON.stringify(source)}</p>
+      <p>result: {JSON.stringify(result)}</p>
+
+      <p>source2: {JSON.stringify(source2)}</p>
+      <p>result2: {JSON.stringify(result2)}</p>
+    </div>
+  );
+};
---
+++ src/validator/isValidNaturalNumber/index.md
@@ -0,0 +1,29 @@
+---
+title: isValidNaturalNumber
+toc: false
+---
+
+## isValidNaturalNumber
+
+判断参数是否为有效自然数
+
+<code src="./demo.tsx"></code>
+
+### API
+
+```typescript
+const flag: boolean = isValidNaturalNumber(num: number);
+```
+
+### Params
+
+| 参数 | 说明 | 类型     | 默认值 |
+| ---- | ---- | -------- | ------ |
+| num  | 参数 | `number` | -      |
+
+
+### Result
+
+| 参数 | 说明             | 类型      | 默认值  |
+| ---- | ---------------- | --------- | ------- |
+| flag | 是否为有效自然数 | `boolean` | `false` |

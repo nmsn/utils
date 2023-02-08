c27db4835a6310d5f58516f5165a5e8369e5987e
nmsn <136696700@qq.com>
2023-02-08 15:07:55 +0800

    style: code sort

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 // rollup.config.js
-import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: 'src/index.ts',

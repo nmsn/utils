fe5f34f551abf4145cdfd7caf02b9e5857e31390
nmsn <136696700@qq.com>
2023-02-26 11:01:43 +0800

    chore: update dumirc

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,21 +5,23 @@ export default defineConfig({
     name: 'Utils',
     nav: [
       // { title: '指南', link: '/guide/test' },
-      { title: 'hooks', link: '/hooks' },
-      { title: 'format', link: '/formats' },
-      { title: 'validator', link: '/validators' },
+      // TODO 目前木知道如何跳转直接导航到目录下第一个，先固定跳转路由
+      { title: 'Hooks', link: '/hooks/useDeepEqualIdentifier' },
+      { title: 'Format', link: '/formats/addThousandSeparator' },
+      { title: 'Validator', link: '/validators/isAsyncFunction' },
       { title: 'GitHub', link: 'https://github.com/nmsn/utils.git' },
     ],
     footer: `Copyright © ${new Date().getFullYear()} nmsn`,
   },
   outputPath: 'docs-dist',
   resolve: {
-    docDirs: ['docs'],
     atomDirs: [
+      { type: 'hooks', dir: '/src/hooks' },
       { type: 'format', dir: 'src/format' },
       { type: 'validator', dir: 'src/validator' },
-      { type: 'hook', dir: '/src/hooks' },
     ],
     codeBlockMode: 'passive',
+    forceKebabCaseRouting: false,
   },
 });
+

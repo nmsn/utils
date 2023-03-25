3da6f0e4ca5f6ad92953a64315fb45f2ea010fd2
nmsn <136696700@qq.com>
2023-03-25 23:28:29 +0800

    feat: add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,7 @@
 export declare type ValueOf<T> = T[keyof T];
 
 export declare type ArrayElement<A> = A extends readonly (infer T)[] ? T : never;
+
+export declare type GetKeyByValueType<T, Condition> = {
+  [K in keyof T]: T[K] extends Condition ? K : never;
+}[keyof T];

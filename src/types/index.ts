9475d9cba3099a67f396e73eee0de04549cae649
nmsn <136696700@qq.com>
2023-07-11 15:13:35 +0800

    feat: add AsyncReturnType type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,5 @@
 import { MutableRefObject } from 'react';
 
-export declare type ValueOf<T> = T[keyof T];
-
-export declare type ArrayElement<A> = A extends readonly (infer T)[] ? T : never;
-
-export declare type GetKeyByValueType<T, Condition> = {
-  [K in keyof T]: T[K] extends Condition ? K : never;
-}[keyof T];
-
 export type TargetValue<T> = T | undefined | null;
 
 export type TargetType = HTMLElement | Element | Window | Document;
@@ -16,3 +8,21 @@ export type BasicTarget<T extends TargetType = Element> =
   | (() => TargetValue<T>)
   | TargetValue<T>
   | MutableRefObject<TargetValue<T>>;
+
+// 获取对象值类型
+export declare type ValueOf<T> = T[keyof T];
+
+// 获取数组的值的类型
+export declare type ArrayElement<A> = A extends readonly (infer T)[] ? T : never;
+
+// 通过对象的值的类型获取 key 的类型
+export declare type GetKeyByValueType<T, Condition> = {
+  [K in keyof T]: T[K] extends Condition ? K : never;
+}[keyof T];
+
+// 获取异步函数返回的结果
+export declare type AsyncReturnType<T extends (...args: any) => any> = T extends (
+  ...args: any
+) => Promise<infer R>
+  ? R
+  : any;
